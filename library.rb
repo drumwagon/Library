af1c9b793008a1f571368e4237e621e11508be17
MakerSquare Student <students@themakersquare.com>
2013-11-19 17:36:35 -0600

    Have Add books method  working properly and list books method working not quite right

--- library.rb
+++ library.rb
@@ -1,9 +1,6 @@
-# 
-# 
 class Library
   def initialize
     @books = []
-
   end
 
   def books
@@ -14,17 +11,25 @@ class Library
 # Will print out list of all books in the library
   def list_books
     @books.each do |books|
-      puts books.title + ' is currently ' #+ book.status 
+      puts books.title + ' is currently ' #@status #+ book.status 
     end
   end
 
-
+# This checks the status of a book in the library and will 
+# show it as borrowed if it is checked out
   def borrowed_books
-    @books.check_out do |books|
+    # if book.status != "Available"
+    #   return book.status = "checked out"
+    # end
   end
 
+# This checks the status of a book in the library and will 
+# show it as available if it is not checked out
   def available_books
-  end
+    # if book.status = "available"
+    #   puts book.title + " is available to check out."
+    # end
+  end 
 
 # Public: Takes each of the book instances added to the "Books" class below and pushes them to 
 # "Books" class below and pushes them to the array of the created Library
@@ -35,12 +40,13 @@ class Library
 
 # Public: Assigns a book in the library to a user and moves it to checked out from the library
   def check_out(user, book)
-    if user.borrowed_books.length == 2
-      return "User already has two books checked out"
-    end
-    if book.statu == "available"
-      book.borrower = user
-      book.status = "checked out"
+    if user.borrowed_books.size < 2 && book.status != "Checked out" && user.standing != "Bad"
+      user.borrowed_books.size = user.borrowed_books.size + 1
+      book.status = "Checked out"
+      user.borrowed_books << borrowed_books
+      book.owner = borrower.name
+    elsif user.borrowed_books.size == 2
+      puts "Sorry, you cannot check out any more books"
     else
       return "Sorry, that book is not available."
     end
@@ -51,6 +57,8 @@ class Library
   end
 end
 
+# created a standing instane
+
 # Public: creates instances of the Borrower class and
 # creats a unique name variable for each borrower
 class Borrower
@@ -59,8 +67,9 @@ class Borrower
     @books = []
   end
 
-# Identifies books that are currently checked out.
+# Identifies books that are currently checked out to a specific user.
   def borrowed_books
+    @borrowed_books << book
   end
 
   def name
@@ -69,9 +78,11 @@ class Borrower
 
 # Defines the number of books checked out
   def borrowed_books_count
+
   end
 
   def borrowed_books_list
+    
   end
 end
 
@@ -94,28 +105,18 @@ class Book
   end
 
   def status
-    @status
+    @status = "Available"
   end
 
-  def status=(new_value)
-    @status = new_value
-  end
+  # def status=(new_value)
+  #   @status = new_value
+  # end
 
-  def borrower
-    @borrower
-  end
-  def borrower(new_value)
-    @borrower = new_value
-  end
+  # def borrower
+  #   @borrower
+  # end
 
-
-
+  # def borrower(new_value)
+  #   @borrower = new_value
+  # end
 end
-
-
-
-
-
-
-
-
